b287b3c7025159b8e62cfc1048a5e8a3d568d7e7
Gimmpy <gimmpy224@gmail.com>
2026-06-15 22:10:22 -0500

    Add initial configuration and main application files

---
+++ config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"embed"
+	"fmt"
+	"os"
+	"gopkg.in/yaml.v3"
+)
+
+type Database struct {
+		Host	 string `yaml:"host"`
+		Port	 int    `yaml:"port"`
+		User	 string `yaml:"user"`
+		Password string `yaml:"password"`
+		Name	 string `yaml:"name"`
+}
+
+func LoadConfig(yamlFile embed.FS) (*Database) {
+	// Read file
+	file, err := yamlFile.ReadFile("config.yaml")
+	if err != nil {
+		fmt.Printf("Error reading config file: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Create Database struct
+	config := &Database{}
+
+	// Unmarshal, which is their stupidass term for SERIALIZE or PARSE, the yaml file into the struct
+	if err := yaml.Unmarshal(file, config); err != nil {
+		fmt.Printf("Error parsing config file: %v\n", err)
+		os.Exit(1)
+	}
+
+	return config
+}
