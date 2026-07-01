b4db85fd268ee5b53ad51e9a6381f3f7a115ef0e
Gimmpy <gimmpy224@gmail.com>
2026-07-01 14:26:43 -0500

    Implement StartMonitoring method in Client struct and update main function to initialize HTTP clients

--- client/client.go
+++ client/client.go
@@ -4,10 +4,12 @@ import (
 	"github.com/Novaic-Solutions/opnsense_monitor/config"
 	"fmt"
 	"encoding/json"
+	"io/ioutil"
 )
 
 type Client struct {
-	
+	ApiRequest *ApiRequest
+	// Probably a channel to send response back on
 }
 
 func PopulateApiRequests(conf *config.Config) (*[]ApiRequest, error) {
@@ -40,5 +42,27 @@ func PopulateApiRequests(conf *config.Config) (*[]ApiRequest, error) {
 	}
 
 	return &apiObj, nil
+}
+
+func (cli *Client) StartMonitoring() {
+	resp, err := cli.ApiRequest.SendRequest()
+	if err != nil {
+		fmt.Printf("Error sending request: %v", err)
+		return
+	}
+	defer resp.Body.Close()
+	
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Error reading response body: %v", err)
+		return
+	}
+
+	fmt.Printf("Response Status: %s\n", resp.Status)
+	fmt.Printf("Response Body: %s\n", string(body))
+
+	// Add logic here to process the response and send it through the channel
+	// for further processing
+
 }
 
--- opnsense_monitor.go
+++ opnsense_monitor.go
@@ -3,7 +3,7 @@ package main
 import (
 	"embed"
 	"fmt"
-	//"os"
+	"os"
 	//"encoding/json"
 	"github.com/Novaic-Solutions/opnsense_monitor/config"
 	//"github.com/Novaic-Solutions/opnsense_monitor/endpoint"
@@ -14,11 +14,27 @@ import (
 var yamlFile embed.FS
 
 func main() {
+	httpClients := make([]*client.Client, 0, 100)
+
 	fmt.Println("Starting application...")
 	conf := config.LoadConfig(yamlFile)
 	fmt.Printf("Loaded config: %+v\n", conf)
 
-	
+	requests, err := client.PopulateApiRequests(conf)
+	if err != nil {
+		fmt.Printf("Error populating API requests: %v\n", err)
+		os.Exit(1)
+	}
+
+	for _, req := range *requests {
+		httpClients = append(httpClients, &client.Client{ApiRequest: &req})
+	}
+
+	// for _, cli := range httpClients {
+	// 	cli.StartMonitoring()
+	// }
+
+	httpClients[0].StartMonitoring()
 
 	// jsonData, err := json.Marshal(req_string)
 	// if err != nil {
