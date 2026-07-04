22f1eb149008452be600de435dd6237c0ac63d0b
Gimmpy <gimmpy224@gmail.com>
2026-07-03 22:14:45 -0500

    Add ResponseData, FirewallState, and FirewallSession structs for data handling

--- data/data.go
+++ data/data.go
@@ -7,6 +7,55 @@ package data
 //  display on the web page.
 //------------------------------------------------------------------------------
 
+type ResponseData struct {
+	rowCount int
+	total int
+	current int
+	rows []any
+}
+
+type FirewallState struct {
+	Label string
+	Descr string
+	Nat_addr string
+	Nat_port string
+	Gateway string
+	Proto string
+	Flags []string
+	Direction string
+	Dst_addr string
+	Dst_port string
+	Src_addr string
+	Src_port string
+	State string
+	Age string
+	Expires string
+	Pkts []int
+	Bytes []int
+	Rule string
+	Id string
+	Interface string
+}
+
+type FirewallSession struct {
+	Proto string
+	Dir string
+	Src_addr string
+	Src_port string
+	Dst_addr string
+	Dst_port string
+	Gw_addr string
+	Gw_port string
+	Age int
+	Expire int
+	Pkts int
+	Bytes int
+	Avg int
+	Rule string
+	Label string
+	Descr string
+}
+
 type DataEntry struct {
 	URI string
 	Timestamp string
