3bd3257d09c1e0579d005496c39fa7a437c15705
Gimmpy <gimmpy224@gmail.com>
2026-07-31 23:02:11 -0500

    Refactor API request handling: comment out SendRequest method and remove CreateApiRequests function for improved clarity and maintainability

--- client/client_request.go
+++ client/client_request.go
@@ -36,67 +36,67 @@ func CreateHTTPClient(timeout int) *http.Client {
 	}
 }
 
-// ------------------------------------------------------------------------------
-// Create Request with Basic Auth and send it
-// ------------------------------------------------------------------------------
-func (apiReq *ApiRequest) SendRequest() (*http.Response, error) {
-	var req *http.Request
-	var err error
+// // ------------------------------------------------------------------------------
+// // Create Request with Basic Auth and send it
+// // ------------------------------------------------------------------------------
+// func (apiReq *ApiRequest) SendRequest() (*http.Response, error) {
+// 	var req *http.Request
+// 	var err error
 
-	//---------------------------------------------------------------------------
-	// Create a new request object with the appropriate method, endpoint, and body.
-	// If the body is empty, set it to nil to prevent sending an empty JSON object.
-	// This should help prevent 400 errors from the API.
-	//---------------------------------------------------------------------------
-	if len(apiReq.Body) == 0 {
-		req, err = http.NewRequest(apiReq.Method, apiReq.Endpoint, nil)
-	} else {
-		//---------------------------------------------------------------------
-		// Ensure only the content type for application/json is set IF
-		// the request body is not empty. This prevents 400 errors from the API.
-		//---------------------------------------------------------------------
-		req, err = http.NewRequest(apiReq.Method, apiReq.Endpoint, bytes.NewBuffer(apiReq.Body))
-		req.Header.Set("Content-Type", "application/json")
-	}
+// 	//---------------------------------------------------------------------------
+// 	// Create a new request object with the appropriate method, endpoint, and body.
+// 	// If the body is empty, set it to nil to prevent sending an empty JSON object.
+// 	// This should help prevent 400 errors from the API.
+// 	//---------------------------------------------------------------------------
+// 	if len(apiReq.Body) == 0 {
+// 		req, err = http.NewRequest(apiReq.Method, apiReq.Endpoint, nil)
+// 	} else {
+// 		//---------------------------------------------------------------------
+// 		// Ensure only the content type for application/json is set IF
+// 		// the request body is not empty. This prevents 400 errors from the API.
+// 		//---------------------------------------------------------------------
+// 		req, err = http.NewRequest(apiReq.Method, apiReq.Endpoint, bytes.NewBuffer(apiReq.Body))
+// 		req.Header.Set("Content-Type", "application/json")
+// 	}
 
-	if err != nil {
-		return nil, err
-	}
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	//---------------------------------------------------------------------------
-	// Set the basic auth for the request using
-	// the token created in opnsense.
-	//---------------------------------------------------------------------------
-	req.SetBasicAuth(apiReq.Username, apiReq.Password)
+// 	//---------------------------------------------------------------------------
+// 	// Set the basic auth for the request using
+// 	// the token created in opnsense.
+// 	//---------------------------------------------------------------------------
+// 	req.SetBasicAuth(apiReq.Username, apiReq.Password)
 	
-	//---------------------------------------------------------------------------
-	// Set the basic auth for the request using
-	// the token created in opnsense.
-	//---------------------------------------------------------------------------
-	req.SetBasicAuth(apiReq.Username, apiReq.Password)
+// 	//---------------------------------------------------------------------------
+// 	// Set the basic auth for the request using
+// 	// the token created in opnsense.
+// 	//---------------------------------------------------------------------------
+// 	req.SetBasicAuth(apiReq.Username, apiReq.Password)
 
-	//----------------------------------------------------------------------------
-	// Create the HTTP client
-	//----------------------------------------------------------------------------
-	client := CreateHTTPClient(30)
+// 	//----------------------------------------------------------------------------
+// 	// Create the HTTP client
+// 	//----------------------------------------------------------------------------
+// 	client := CreateHTTPClient(30)
 
-	//----------------------------------------------------------------------------
-	// Send the request and return the response
-	//----------------------------------------------------------------------------
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
+// 	//----------------------------------------------------------------------------
+// 	// Send the request and return the response
+// 	//----------------------------------------------------------------------------
+// 	resp, err := client.Do(req)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	fmt.Printf("Endpoint.go -- Request sent to %s with method %s\n", apiReq.Endpoint, apiReq.Method)
-	fmt.Printf("Endpoint.go -- Response Status: %s\n", resp.Status)
+// 	fmt.Printf("Endpoint.go -- Request sent to %s with method %s\n", apiReq.Endpoint, apiReq.Method)
+// 	fmt.Printf("Endpoint.go -- Response Status: %s\n", resp.Status)
 
-	// Check if the response body is empty and print it for debugging purposes.
-	if apiReq.Body != nil {
-		fmt.Printf("Endpoint.go -- Request Body: %s\n", string(apiReq.Body))
-	} else {
-		fmt.Printf("Endpoint.go -- Request Body: nil\n")
-	}
+// 	// Check if the response body is empty and print it for debugging purposes.
+// 	if apiReq.Body != nil {
+// 		fmt.Printf("Endpoint.go -- Request Body: %s\n", string(apiReq.Body))
+// 	} else {
+// 		fmt.Printf("Endpoint.go -- Request Body: nil\n")
+// 	}
 
-	return resp, nil
-}
+// 	return resp, nil
+// }
--- opnsense_monitor.go
+++ opnsense_monitor.go
@@ -22,30 +22,6 @@ import (
 var yamlFile embed.FS
 
 
-//------------------------------------------------------------------------------
-//	 Create the request objects for each of the API endpoints
-//------------------------------------------------------------------------------
-// func CreateApiRequests(conf *config.Config, httpClients *[]*client.Client, responseChannel chan client.EndpointResponse) error {
-// 	//-------------------------------------------------------------------
-// 	//     Create the request objects for each of the API endpoints
-// 	//     then create a client for each of the requests and start 
-// 	// 	   monitoring
-// 	//-------------------------------------------------------------------
-// 	requests, err := client.PopulateApiRequests(conf)
-// 	if err != nil {
-// 		fmt.Printf("Opnsense_monitor.go -- Error populating API requests: %v\n", err)
-// 		return err
-// 	}
-
-// 	//-------------------------------------------------------------------
-// 	//    Create a client for each of the requests and start monitoring
-// 	//-------------------------------------------------------------------
-// 	for _, req := range *requests {
-// 		*httpClients = append(*httpClients, &client.Client{ApiRequest: &req, ResponseChannel: responseChannel})
-// 	}
-// 	return nil
-// }
-
 //----------------------------------------------------------------------------
 //	Initialize the application, load the configuration, create the API requests,
 //  and start the monitoring clients.
