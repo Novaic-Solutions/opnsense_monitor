4e811debc2e97f763b2f70fb014ec369a42e0984
Gimmpy <gimmpy224@gmail.com>
2026-08-14 20:40:54 -0500

    Add documentation for Prometheus metrics naming conventions in data_handler.go

--- data/data_handler.go
+++ data/data_handler.go
@@ -4,3 +4,14 @@ import (
 	//"fmt"
 	//"github.com/Novaic-Solutions/opnsense_monitor/config"
 )
+
+// Nameing convention for the prometheus metrics
+// prometheus_metric_name{label="value"} value
+// 
+// For each type of metric, a line will be generated as follows:
+// # HELP prometheus_metric_name description of the metric including the measurement units
+// # TYPE prometheus_metric_name type of metric (counter, gauge, histogram, summary)
+
+
+//----------------------------------------------------------------------------
+// Metrics for 
