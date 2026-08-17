ba3d84daa4aa6711490954cb05b34f9f3b1cde5d
Gimmpy <gimmpy224@gmail.com>
2026-08-16 21:29:39 -0500

    Refactor ARP table processing in data_handler.go: extract GetArpEntryTitle method and enhance ProcessArpTableEntry logic

--- data/data_handler.go
+++ data/data_handler.go
@@ -70,9 +70,29 @@ func (dh *DataHandler) HandleIncomingData() {
 // Prometheus metrics will be generated for each entry in the ARP table as follows:
 // arp_table_entry{ip="", mac="", interface="", manufacturer="", hostname=""} 1
 //-------------------------------------------------------------------------------------------------------------------
-func (dh *DataHandler) ProcessArpTableData(arpTable ArpTable) {
+func (dh *DataHandler) GetArpEntryTitle(arpEntry ArpTableEntry) string {
 	// Create the key string for the prometheus metric
-	
+	keyTitle := "arp_table_entry{ip=\"" + arpEntry.Ip + "\", mac=\"" + arpEntry.Mac + "\", interface=\"" + arpEntry.Interface + "\", manufacturer=\"" + arpEntry.Manufacturer + "\", hostname=\"" + arpEntry.Hostname + "\"}"
+	return keyTitle
+}
+
+func (dh *DataHandler) ProcessArpTable(arpTable ArpTable) {
+	for _, arpEntry := range arpTable.Rows {
+		dh.ProcessArpTableEntry(arpEntry)
+	}
+}
+
+func (dh *DataHandler) ProcessArpTableEntry(arpEntry ArpTableEntry) {
+	// Create the key string for the prometheus metric
+	keyTitle := dh.GetArpEntryTitle(arpEntry)
+
+	// Check if the key already exiztzs in the Metrics map, if not, 
+	// add it with a value of 1, if it does exist, increment the value by 1
+	if _, exists := dh.Metrics[keyTitle]; !exists {
+		dh.Metrics[keyTitle] = 1
+	} else {
+		dh.Metrics[keyTitle]++
+	}
 }
 
 
